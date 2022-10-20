8a1cad261e4b0b70f06c130ed441f0f30b4b0abe
vanessa <vanessa@example.com>
2022-10-20 09:59:33 +0200

    corretto es.1

--- Esercizio_1/script/script.js
+++ Esercizio_1/script/script.js
@@ -1,10 +1,10 @@
-document.querySelectorAll('#div p')
+document.querySelectorAll('div p')
     .forEach(e => e.addEventListener("mouseover", function () {
         if (e.className == "odd") {
             e.style.color ="red";
         } 
     }));
-    document.querySelectorAll('#div p')
+    document.querySelectorAll('div p')
     .forEach(e => e.addEventListener("mouseout", function () {
         e.style.color ="black";
     }));
