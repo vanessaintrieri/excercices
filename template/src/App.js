3a8d2ef119ae7017e9d3e74e6f3082d16b3bf572
vanessa <vanessa@example.com>
2022-10-24 13:15:58 +0200

    template

---
+++ template/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import CustomForm from './components/forms/CustomForm'
+
+function App() {
+  return (
+    <div className="App">
+      <CustomForm></CustomForm>
+    </div>
+  );
+}
+
+export default App;
