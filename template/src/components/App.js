1685a3df5b5cc56b1a906a32d7ef0b0df357cd2c
vanessa <vanessa@example.com>
2022-10-25 12:05:17 +0200

    commit template

---
+++ template/src/components/App.js
@@ -0,0 +1,9 @@
+import '../css/style.scss';
+
+function App() {
+  return (
+    <h1>Hello world</h1>
+  );
+}
+
+export default App;
--- template/src/index.js
+++ template/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import './css/style.scss';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
